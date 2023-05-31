540443f21610250db039ef2d4f7d21c8cce88841
Andrey <tungulin2002@gmail.com>
2023-05-31 15:34:17 +0300

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import Home from "./pages/Home"
+import './styles/variables.scss'
+
+
+const App: FC = () => {
+  return <Home />
+}
+
+export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+import './Home.scss'
+
+const Home: FC = () => {
+    return <div className='home'>Home</div>
+}
+
+export default Home
